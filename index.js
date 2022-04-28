1c41b90611e4d19bf4ff7ae42cb200d4801448f8
aymkhDnD <aymen.khedhria@dndserv.com>
2022-04-29 00:46:53 +0100

    New version

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-const cardDisplay = document.querySelector('#grid-container')
-const links = document.querySelector('#socials')
-const btn = document.querySelector('#contactButton')
 
-// contact links bounce
-btn.addEventListener('click', ()=>{
-    links.classList.add("animate__bounce", "animate__animated")
-    setTimeout(() => {
-        links.classList.remove("animate__bounce", "animate__animated")        
-    }, 1000);
-    
-})
