8c613996eb9eeaef2fc2ea4f1e2019bc2feab39c
aymkh <aymen4real@gmail.com>
2022-07-26 13:44:04 +0100

    bio, stack, layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+// MATERIAL
+import {MatTabsModule} from '@angular/material/tabs';
+import { HotLinksComponent } from './components/hot-links/hot-links.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { MainTabsComponent } from './components/main-tabs/main-tabs.component';
+import { BioComponent } from './components/main-tabs/bio/bio.component';
+import { PortfolioComponent } from './components/main-tabs/portfolio/portfolio.component';
+import { StackComponent } from './components/main-tabs/stack/stack.component';
+import {MatExpansionModule} from '@angular/material/expansion';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HotLinksComponent,
+    MainTabsComponent,
+    BioComponent,
+    PortfolioComponent,
+    StackComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTabsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
