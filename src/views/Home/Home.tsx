d14ca9677e53e6ebfb897171efde5336fb78568a
aymkh <aymen4real@gmail.com>
2022-10-24 10:35:52 +0100

    Basic layout config

---
+++ src/views/Home/Home.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import HomeLayout from './HomeStyles'
+
+export default function Home() {
+    return (
+        <HomeLayout>Home</HomeLayout>
+    )
+}
---
+++ src/views/Home/HomeStyles.jsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const HomeLayout = styled.section`
+    
+`;
+
+export default HomeLayout;
