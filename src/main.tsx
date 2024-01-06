993cb2e8b6e472ea90cb2c32587b14047741f7af
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2024-01-06 10:39:37 +0100

    fix: tailwind setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { NextUIProvider } from '@nextui-org/react'
+import "./index.css"
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <NextUIProvider>
-      <App />
-    </NextUIProvider>
+    <App />
   </React.StrictMode>
 )
